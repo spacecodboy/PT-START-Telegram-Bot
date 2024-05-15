655e84c232ea304b3505ea46bee11190e9b7956f
spacecodboy <saschenko41@gmail.com>
2024-05-15 22:11:53 +0700

    edit postgres_db

--- postgres_db.py
+++ postgres_db.py
@@ -33,8 +33,8 @@ while connection == None:
     logging.error("Выполняется подключение к базе данных.")
     connection = createConnection(
         os.getenv('DB_DATABASE'), os.getenv('DB_USER'), os.getenv('DB_PASSWORD'), os.getenv('DB_HOST'), os.getenv('DB_PORT')
-    break;
-)
+    )
+    break
 
 def executeReadQuery(connection, query):
     cursor = connection.cursor()
